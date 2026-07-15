4fa878043cc0d9f94d5d9b4f60373902ffdcaf04
nakzgeo69 <nakzgeo@gmail.com>
2026-07-15 19:47:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // ===== PASTEBIN CONFIG URL =====
 // Replace with your actual Pastebin RAW URL
-const PASTEBIN_CONFIG_URL = 'https://pastebin.com/raw/V5xWLwFL';
+const PASTEBIN_CONFIG_URL = 'https://pastebin.com/raw/5Rmg9VNM';
 
 // ===== CONFIG CACHE =====
 let cachedConfig = null;
